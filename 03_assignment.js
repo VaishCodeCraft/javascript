b2786ca08e7ce3ee6dcb1bede24b2d3b70113e4a
vaishnavi shivaji nagargoje <vaishnavinagargoje996@gmail.com>
2026-07-20 12:35:08 +0700

    added example in comparison operator

---
+++ 03_assignment.js
@@ -0,0 +1,18 @@
+let x = 10;
+x += 5;       
+console.log(x); // Outputs: 15
+
+x -= 3;       
+console.log(x); // Outputs: 12
+
+x *= 2;       
+console.log(x); // Outputs: 24
+
+x /= 4;       
+console.log(x); // Outputs: 6
+
+x %= 3;       
+console.log(x); // Outputs: 0
+
+x **= 2;      
+console.log(x); // Outputs: 0
---
+++ 04_comparison.js
@@ -0,0 +1,11 @@
+let a = 70;
+let b = 30;
+
+console.log(a == b);   // false (70 is not equal to 30)
+console.log(a === b);  // false (70 is not strictly equal to 30)
+console.log(a != b);   // true  (70 is not equal to 30)
+console.log(a !== b);  // true  (70 is not strictly equal to 30)
+console.log(a > b);    // true  (70 is greater than 30)
+console.log(a < b);    // false (70 is not less than 30)
+console.log(a >= b);   // true  (70 is greater than or equal to 30)
+console.log(a <= b);   // false (70 is not less than or equal to 30)
